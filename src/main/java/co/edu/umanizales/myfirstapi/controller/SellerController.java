adc2f617074656187364b67991f71c45413ac2ac
Daniela Cifuentes <dcifuentes109623@umanizales.edu.co>
2025-03-26 21:14:41 -0500

    Cambio en el genero y location de 4 seller. Se incluyen 4 Location

--- src/main/java/co/edu/umanizales/myfirstapi/controller/SellerController.java
+++ src/main/java/co/edu/umanizales/myfirstapi/controller/SellerController.java
@@ -15,11 +15,14 @@ public class SellerController {
     @GetMapping
     public List<Seller> getSeller() {
         Location manizales = new Location("17001", "Manizales");
-        Seller daniela = new Seller("1053854324", "Daniela", "Cifuentes Rendón", (char) 1, (byte) 28, manizales);
-        Seller adriana = new Seller("30331751", "Adriana María", "Rendoón Gómez", (char) 1, (byte) 50, manizales);
-        Seller andres = new Seller("1060649816", "Andrés", "Taborda Posada", (char) 2, (byte) 34, manizales);
-        Seller bertha = new Seller("24316690", "Bertha Lucia", "Gómez Aguirre", (char) 1, (byte) 69, manizales);
-        Seller valery = new Seller("1002810745", "Valery", "Velasquez Cifuentes", (char) 1, (byte) 12, manizales);
+        Location pereira = new Location("66001", "Pereira");
+        Location risaralda = new Location("17616", "Risaralda");
+        Location aranzazu = new Location("17050", "Aranzazu");
+        Seller daniela = new Seller("1053854324", "Daniela", "Cifuentes Rendón", 'F', (byte) 28, manizales);
+        Seller adriana = new Seller("30331751", "Adriana María", "Rendoón Gómez", 'F', (byte) 50, manizales);
+        Seller andres = new Seller("1060649816", "Andrés", "Taborda Posada", 'M', (byte) 34, risaralda);
+        Seller bertha = new Seller("24316690", "Bertha Lucia", "Gómez Aguirre", 'F', (byte) 69, aranzazu);
+        Seller valery = new Seller("1002810745", "Valery", "Velasquez Cifuentes", 'F', (byte) 12, pereira);
 
         return List.of(daniela, adriana, andres, bertha, valery);
     }
