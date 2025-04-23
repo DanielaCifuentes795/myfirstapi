7e25fc89fed687a6db0a77b6d7003e6a9f4cb47b
Daniela Cifuentes <dcifuentes109623@umanizales.edu.co>
2025-04-23 14:05:56 -0500

    Se incluyen métodos en la pestaña LocationController y código en la pestaña Location Service, con el fin de retornar información relacionada con el archivo CSV cargado el cual contiene datos de los municipios de Colombia. Se crea una nueva clase llamada State para retornar especialmente información de los departamentos a través de los métodos

---
+++ src/main/java/co/edu/umanizales/myfirstapi/model/State.java
@@ -0,0 +1,13 @@
+package co.edu.umanizales.myfirstapi.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class State {
+    private String code;
+    private String name;
+}
